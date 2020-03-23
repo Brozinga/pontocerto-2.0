640073985ca2f3328b9b2cb7f4a34577cd212789
Fernando Brozingamoraemoraeddddasafs-zxc <fbrozinga@outlook.coma1234>
2020-03-23 02:24:39 -0300

    Mudando a identidade visual do Front

--- front-end/src/components/Logo/index.jsx
+++ front-end/src/components/Logo/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import image_logo from '../../assets/images/Projeto-PontoCerto.png';
+import image_logo from '../../assets/images/logo-white.png';
 
 export default function Logo() {
     return <img src={image_logo} alt="Logo - Ponto Certo" />
